a1583d21aad8cc0589609a9a03201bbd48a1b987
dandeeccastro <dundeepessoal@gmail.com>
2022-07-30 13:34:41 -0300

    urna

---
+++ js/util.js
@@ -0,0 +1,11 @@
+function ajax(url, method, callback, body = null) {
+  let request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open(method, url, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status == "200") {
+        callback(request.responseText);
+    }
+  };
+  request.send(body);
+}
